11b0585bb302a6a24adc64a7984ef1d47c11abc4
Basavanagowda Kanur <basavanagowda@gmail.com>
2020-12-13 10:22:30 +0530

    Move transformation of laureates & id-based indexing to fetch module

--- frontend/src/fetch/index.ts
+++ frontend/src/fetch/index.ts
@@ -13,6 +13,28 @@ const normalizeBirthInfo = (laureate: any) => ({
       },
 });
 
+const getFullName = (laureate: any) =>
+  laureate.knownName
+    ? laureate.knownName
+    : `${laureate.givenName} ${laureate.familyName}`;
+const getOrgName = (laureate: any) => laureate.orgName;
+const getName = (laureate: any) =>
+  getOrgName(laureate) || getFullName(laureate);
+
+const getFoundedCountry = (laureate: any) =>
+  laureate.founded && laureate.founded.place
+    ? laureate.founded.place.countryNow
+    : undefined;
+const getBirthCountry = (laureate: any) =>
+  laureate.birth && laureate.birth.place
+    ? laureate.birth.place.countryNow
+    : undefined;
+const getCountry = (laureate: any) =>
+  getFoundedCountry(laureate) || getBirthCountry(laureate);
+
+const getAwards = (laureate: any) =>
+  laureate.nobelPrizes.map((p: any) => p.awardYear).join(', ');
+
 export default (offset: number = 0) => {
   return axios
     .get(`https://api.nobelprize.org/2.0/laureates?limit=25&offset=${offset}`)
@@ -27,6 +49,25 @@ export default (offset: number = 0) => {
       ...rest,
       laureates: (laureates as any[]).map((laureate) =>
         normalizeBirthInfo(laureate)
+      ),
+    }))
+    .then(({ laureates, ...rest }) => ({
+      ...rest,
+      laureates: (laureates as any[]).map((laureate: any) => ({
+        ...laureate,
+        name: getName(laureate),
+        country: getCountry(laureate),
+        awards: getAwards(laureate),
+      })),
+    }))
+    .then(({ laureates, ...rest }) => ({
+      ...rest,
+      laureates: laureates.reduce(
+        (acc: any, laureate: any) => ({
+          ...acc,
+          [laureate.id]: laureate,
+        }),
+        {}
       ),
     }));
 };
--- frontend/src/reducer.ts
+++ frontend/src/reducer.ts
@@ -77,28 +77,6 @@ const groupByCategory = (laureates: Laureates): Category[] => {
   }));
 };
 
-const getFullName = (laureate: any) =>
-  laureate.knownName
-    ? laureate.knownName
-    : `${laureate.givenName} ${laureate.familyName}`;
-const getOrgName = (laureate: any) => laureate.orgName;
-const getName = (laureate: any) =>
-  getOrgName(laureate) || getFullName(laureate);
-
-const getFoundedCountry = (laureate: any) =>
-  laureate.founded && laureate.founded.place
-    ? laureate.founded.place.countryNow
-    : undefined;
-const getBirthCountry = (laureate: any) =>
-  laureate.birth && laureate.birth.place
-    ? laureate.birth.place.countryNow
-    : undefined;
-const getCountry = (laureate: any) =>
-  getFoundedCountry(laureate) || getBirthCountry(laureate);
-
-const getAwards = (laureate: any) =>
-  laureate.nobelPrizes.map((p: any) => p.awardYear).join(', ');
-
 export default (state: State = initialState, action: any): State => {
   switch (action.type) {
     case FETCHING_LAUREATES:
@@ -108,19 +86,8 @@ export default (state: State = initialState, action: any): State => {
     case RECEIVED_LAUREATES:
       return {
         ...state,
-        done: state.done + action.payload.length,
-        laureates: action.payload.reduce(
-          (acc: Laureates, laureate: Laureate) => ({
-            ...acc,
-            [laureate.id]: {
-              ...laureate,
-              name: getName(laureate),
-              country: getCountry(laureate),
-              awards: getAwards(laureate),
-            },
-          }),
-          { ...state.laureates }
-        ),
+        done: state.done + Object.getOwnPropertyNames(action.payload).length,
+        laureates: { ...state.laureates, ...action.payload },
       };
     case DONE_FETCHING: {
       return {
