64f1c9c3ccbba05ee39881baae66517fd0c87490
Basavanagowda Kanur <basavanagowda@gmail.com>
2021-06-29 14:42:54 +0530

    Rename application module as Nobel

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ require_relative '../lib/proxy_to_frontend'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module NobelPrizes
+module Nobel
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
